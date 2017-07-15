79fed0eb2a79500f8e40d5a129ee2b22c85feafb
Deepak Sharma <sharma1725@gmail.com>
2017-07-15 17:49:01 +0530

    IDE Compiled files removed and unused methods removed

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,5 @@ app_name = 'users'
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='index'),
     url(r'^register/$', views.Register.as_view(), name='register'),
-    url(r'^login/$', views.ResultsView.as_view(), name='login'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
 ]
--- users/views.py
+++ users/views.py
@@ -20,25 +20,6 @@ class Register(generic.TemplateView):
     template_name = 'crud/register.html'
 
 
-class ResultsView(generic.ListView):
+class LoginView(generic.ListView):
     model = Question
     template_name = 'crud/login.html'
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'crud/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
