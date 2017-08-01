89c73396da89066f28ea6e8805bfea172ba60526
Deepak Sharma <sharma1725@gmail.com>
2017-08-01 21:06:09 +0530

    Settings updated to postgres

--- crudapi/settings.py
+++ crudapi/settings.py
@@ -93,18 +93,18 @@ WSGI_APPLICATION = 'crudapi.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'myproject',
-    #     'USER': 'deepak',
-    #     'PASSWORD':'123456',
-    #     'HOST':'localhost',
-    #     'PORT': '',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'deepak',
+        'PASSWORD':'123456',
+        'HOST':'localhost',
+        'PORT': '',
+    }
 }
 
 
