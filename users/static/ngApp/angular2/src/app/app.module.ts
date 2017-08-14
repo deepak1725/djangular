233dfe6d65c245fd766f034e8cc99dff30c7cc30
Deepak Sharma <sharma1725@gmail.com>
2017-08-14 21:36:02 +0530

    Angular Material theme to Login Added.

--- users/static/ngApp/angular2/src/app/app.module.ts
+++ users/static/ngApp/angular2/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MainRouterModule, routingMethods } from './main.router.module';
 import {UserService} from './_services/user.service';
 import {AuthenticationService} from './_services/authentication.service';
 import { Http} from '@angular/http';
 import { HttpModule } from '@angular/http';
+import {MyOwnCustomMaterialModule} from './material.module';
+import 'hammerjs';
 
 
 @NgModule({
@@ -18,8 +20,10 @@ import { HttpModule } from '@angular/http';
     BrowserModule,
     FormsModule,
     MainRouterModule,
-    HttpModule
-  ],
+    HttpModule,
+    MyOwnCustomMaterialModule,
+    ReactiveFormsModule
+    ],
   providers: [UserService, AuthenticationService],
   bootstrap: [AppComponent]
 })
--- users/static/ngApp/angular2/src/app/auth/login/login.component.ts
+++ users/static/ngApp/angular2/src/app/auth/login/login.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AuthenticationService } from '../../_services/authentication.service';
-
 import {UserService} from '../../_services/user.service';
+import { NgForm } from '@angular/forms';
+import { FormGroup,FormBuilder,FormControl,Validators } from '@angular/forms';
+import {MdSnackBar} from '@angular/material';
+
 
 
 @Component({
@@ -11,33 +14,66 @@ import {UserService} from '../../_services/user.service';
 
 })
 export class LoginComponent implements OnInit{
-  loading = false;
-  returnUrl: string;
   title = 'Login';
-  model: any = {};
+  addForm: FormGroup;
+
 
   constructor(
+        private formBuilder: FormBuilder,
         private route: ActivatedRoute,
         private router: Router,
-        private authenticationService: AuthenticationService) { }
+		private authenticationService: AuthenticationService,
+		public snackBar: MdSnackBar
+	) { }
 
-  ngOnInit() {
-        // reset login status
-      //   this.authenticationService.logout();
- 
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-  }
+	ngOnInit() {
+		this.addForm = this.formBuilder.group({
+			username: ['', Validators.required],
+			password: ['', Validators.required],
+		})
+	};
 
-  login(details: any){
-  console.log(details);
-  this.loading = true;
-  this.authenticationService.login(details.username, details.password)
-      .subscribe(
-         function(response){ 
-             console.log("Success response", response)
-            },
-         function(response){ console.log("Error happened", response )}
-        );
-  }
+	emailFormControl = new FormControl('', [
+		Validators.required,
+	]);
+	passwordFormControl = new FormControl('', [
+		Validators.required,
+	]);
+	openSnackBar = function (message){
+		console.log(message);
+		this.snackBar.open(message," ", {
+			duration: 2000,
+		}); 
+	};
+	
+	loginUser(){
+		var adduser = {
+			username: this.addForm.controls['username'].value,
+			password: this.addForm.controls['password'].value,    
+		};
+
+		console.log(adduser);
+		let that = this;
+
+		this.authenticationService.login(adduser.username, adduser.password)
+			.subscribe(
+				function(response){
+					console.log(response);
+					// response = response.json();
+					that.openSnackBar("Successfully logged in.")
+
+					// that.openSnackBar(response.non_field_errors)
+					// this.snackBar.open("PizzaPartyComponent"," ", {
+					// 	duration: 500,
+					// }); 
+							
+					console.log("Success response", response)
+				},
+				function(response){ 
+					response = response.json();
+					that.openSnackBar(response[Object.keys(response)[0]])
+					console.log("Error happened", response )
+				}
+			);
+	}
 }
