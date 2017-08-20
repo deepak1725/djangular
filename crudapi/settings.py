626bd2ee88c12db0ee47db449873c4a12f22ac0d
Deepak Sharma <sharma1725@gmail.com>
2017-08-20 21:40:53 +0530

    heroku

--- crudapi/settings.py
+++ crudapi/settings.py
@@ -213,6 +213,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = "media"
+MEDIA_URL = '/media/'
+
 # STATICFILES_DIRS = (
 #     os.path.join(PROJECT_ROOT, 'static'),
 # )
--- crudapi/urls.py
+++ crudapi/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
 
     url(r'^', include('users.urls', namespace='users')),
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
