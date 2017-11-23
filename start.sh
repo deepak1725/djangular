401dd0d10246e97f7b1b32529a3e9b2c461143aa
Deepak Sharma <deepak.sharma@ucodesoft.com>
2017-11-23 18:35:20 +0530

    docker config

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@
 python manage.py collectstatic --noinput 
 python manage.py makemigrations
 python manage.py migrate
+npm --version
 gunicorn crudapi.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3
