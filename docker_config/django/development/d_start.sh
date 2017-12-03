1fbd075de3b056af63f7e9d1ae035b4a2e026568
Deepak Sharma <sharma1725@gmail.com>
2017-12-03 21:25:06 +0530

    nginx produc n dev

--- docker_config/django/development/d_start.sh
+++ docker_config/django/development/d_start.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 echo "Starting Django Scriptt"
 
-nodejs -v && npm -v
 python3 manage.py collectstatic --noinput 
 python3 manage.py makemigrations
 python3 manage.py migrate
-npm --version
-echo "Starting Node:"
-npm install
+
 gunicorn crudapi.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3
