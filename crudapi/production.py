0425f8b7879d8dd898bb0d0e1bffa552c683898c
Deepak Sharma <sharma1725@gmail.com>
2018-04-03 21:18:39 +0530

    master config

--- crudapi/production.py
+++ crudapi/production.py
@@ -213,14 +213,20 @@ ACCOUNT_EMAIL_REQUIRED =  True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = "media"
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'local_statics'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 
 DATABASES['default'].update(db_from_env)
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- crudapi/settings.py
+++ crudapi/settings.py
@@ -213,7 +213,6 @@ ACCOUNT_EMAIL_REQUIRED =  True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
 
@@ -223,12 +222,11 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'local_statics'),
 )
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'my_cdn')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 
 DATABASES['default'].update(db_from_env)
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
